5de493b59d6215db06b41d35e2c93bc9081000a3
syedhaseeb1 <haseebumerzai@gmail.com>
2024-02-12 15:17:26 +0500

    release v1.0.0

---
+++ README.md
@@ -0,0 +1,95 @@
+# Typing-Text Library
+
+Typing-Text is a library that provides a typewriter-like animation effect for your Android TextView.
+
+## Installation
+### Pre-Requisites
+
+* Android 13 support
+* Minumum SDK 21
+* Built with 7.2 Gradle
+***
+
+### Gradle (build.gradle)
+
+#### Groovy
+
+```groovy
+repositories {
+    mavenCentral()
+    maven { url 'https://jitpack.io' }
+}
+
+dependencies {
+    implementation 'com.github.syedhaseeb1:Typing-Text:$latestVersion'
+}
+```
+
+#### KTS
+```KTS
+repositories {
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    implementation("com.github.syedhaseeb1:Typing-Text:$latestVersion")
+}
+```
+## Usage in XML
+````
+<com.hsb.typingtext.TypingTextView
+    android:id="@+id/typing-Text"
+    android:layout_width="0dp"
+    android:layout_height="wrap_content"
+    android:padding="@dimen/_10sdp"
+    android:text="@string/something_went_bazinngistan"
+    android:textAlignment="textStart"
+    android:textColor="#000000"
+    android:textSize="16sp"
+    app:animSpeed="20"
+    app:fadeOnEnd="true"
+    app:layout_constraintBottom_toTopOf="@+id/startBtn"
+    app:layout_constraintEnd_toEndOf="parent"
+    app:layout_constraintStart_toStartOf="parent"
+    app:repeatMode="restart"
+    app:reversed="false" />
+````
+
+### XML Attributes
+````
+app:animSpeed:"int" 
+Sets the animation speed (default is 20).
+````
+
+````
+app:fadeOnEnd:"boolean" 
+Enables fading of the text at the end of the animation (default is true).
+````
+````
+app:repeatMode:"mode" 
+Sets the repeat mode of the animation (default is restart).
+restart, reversed, noRepeat
+````
+
+````
+app:reversed:"boolean" 
+Reverses the animation if true (default is false).
+````
+
+## Usage in Code
+````
+typingText.setTextToAnimate("your_text")
+
+````
+## Contributing
+
+If you'd like to contribute to this project, please follow the [Contribution Guidelines](CONTRIBUTING.md).
+
+
+## License
+
+This project is licensed under the [GNU General Public License, Version 3.0](http://www.gnu.org/licenses/#GPL). Any fork of
+this project is requested to maintain the same license as per the law.
+
+Please see the [LICENSE](LICENSE.md) file for full reference.
---
+++ typingtextview/src/main/java/com/hsb/typingtext/TypingTextView.kt
@@ -0,0 +1,137 @@
+package com.hsb.typingtext
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import android.animation.ValueAnimator
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+
+/**
+ * Developed by Syed Haseeb
+ * Updated on Feb 12, 2024
+ *
+ * XML Usage:
+ *
+ *   <com.hsb.typing.TypingTextView
+ *         android:id="@+id/typingTextView"
+ *         android:layout_width="0dp"
+ *         android:layout_height="wrap_content"
+ *         android:padding="@dimen/_10sdp"
+ *         android:text="@string/something_went_bazinngistan"
+ *         android:textAlignment="textStart"
+ *         android:textColor="#000000"
+ *         android:textSize="16sp"
+ *         app:animSpeed="20"
+ *         app:fadeOnEnd="true"
+ *         app:layout_constraintBottom_toTopOf="@+id/startBtn"
+ *         app:layout_constraintEnd_toEndOf="parent"
+ *         app:layout_constraintStart_toStartOf="parent"
+ *         app:repeatMode="restart"
+ *         app:reversed="false" />
+ *
+ */
+class TypingTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = android.R.attr.textViewStyle
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    private var textToAnimate: CharSequence = ""
+    private var index = 0
+    private val handler = Handler(Looper.getMainLooper())
+    private var typingSpeedMillis: Long = 100
+    private var repeatMode: Int = ValueAnimator.RESTART
+    private var reversed: Boolean = false
+    private var fadeOnEnd: Boolean = false
+
+    private var fadeAnim: Animation
+
+    //Repeat Modes
+    private val repeat = 0
+    private val reverse = 1
+    private val noRepeat = 2
+
+    init {
+        // Retrieve the initial text from the default TextView
+        textToAnimate = "$text"
+        text = null
+        fadeAnim = AnimationUtils.loadAnimation(context, R.anim.fade_out)
+
+        // Retrieve custom attributes
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.TypingTextView)
+        typingSpeedMillis = typedArray.getInt(
+            R.styleable.TypingTextView_animSpeed,
+            100
+        ).toLong()
+        repeatMode = typedArray.getInt(
+            R.styleable.TypingTextView_repeatMode,
+            ValueAnimator.RESTART
+        )
+        reversed = typedArray.getBoolean(R.styleable.TypingTextView_reversed, false)
+        fadeOnEnd = typedArray.getBoolean(R.styleable.TypingTextView_fadeOnEnd, false)
+        typedArray.recycle()
+
+        startTypingAnimation()
+    }
+
+    private fun startTypingAnimation() {
+        if (reversed) {
+            reverseText()
+        }
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                if (index < textToAnimate.length) {
+                    append(textToAnimate[index].toString())
+                    index++
+                    handler.postDelayed(this, typingSpeedMillis)
+                } else {
+                    if (fadeOnEnd) {
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            this@TypingTextView.startAnimation(fadeAnim)
+                        }, 800)
+                    }
+                    when (repeatMode) {
+                        repeat -> {
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                index = 0
+                                this@TypingTextView.text = ""
+                                handler.postDelayed(this, typingSpeedMillis)
+                            }, 1500)
+                        }
+
+                        reverse -> {
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                index = textToAnimate.length
+                                this@TypingTextView.text = ""
+                                handler.postDelayed(this, typingSpeedMillis)
+                            }, 1500)
+                        }
+
+                        noRepeat -> {
+                            index = 0
+                        }
+                    }
+
+                }
+            }
+        }, 1000)
+    }
+
+    private fun reverseText() {
+        val originalText = textToAnimate
+        val reversedText = originalText.reversed()
+        textToAnimate = reversedText
+    }
+
+    // Public method to update the text to animate
+    fun setTextToAnimate(text: CharSequence) {
+        this.textToAnimate = text
+        index = 0
+        this.text = null
+        startTypingAnimation()
+    }
+}
+
